fa6f75f5d188073d47621bd08ed69b7366cb9563
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-21 20:17:06 +0530

    Added: Welcome message

---
+++ src/main/java/com/bdlz/collectionaddressbook/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.bdlz.collectionaddressbook;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Address Book program");
+    }
+}
